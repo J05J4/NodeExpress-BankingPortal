9e82a64b14591cdd02aaa9ec703b644add85d074
XD99VU <josja.heerema@ing.com>
2018-09-11 14:33:24 +0200

    File Handling and Routing

--- src/app.js
+++ src/app.js
@@ -10,6 +10,25 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.render('index', { title: 'Index' }));
+function readJsonFile(fileName) {
+    return fs.readFileSync('src/json/' + fileName + '.json', 'utf8', (error, data) => {
+        if (error) {
+            console.error(error);
+        } else {
+            return data;
+        }
+    });
+}
+const accountData = readJsonFile('accounts');
+const accounts = JSON.parse(accountData);
+
+const userData = readJsonFile('users');
+const users = JSON.parse(userData);
+
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
+app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
+app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
+app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
+app.get('/profile', (req, res) => {res.render('profile', {user: users[0]})});
 
 app.listen(3000, () => console.log('PS Project Running on port 3000!')); 
