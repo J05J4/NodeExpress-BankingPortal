6af368fe089beebce4d0afc79d206d115d9b9a81
XD99VU <josja.heerema@ing.com>
2018-09-13 09:29:03 +0200

    Fix for module 4: tests fail if functionallity is in function

--- src/routes/services.js
+++ src/routes/services.js
@@ -22,8 +22,24 @@ function handlePayment(req, res) {
 }
 
 router.get('/transfer', (req, res) => res.render('transfer'));
-router.post('/transfer', (req, res) => handleTransfer(req, res));
+router.post('/transfer', (req, res) => {
+    accounts[req.body.from].balance -= parseInt(req.body.amount);
+    accounts[req.body.to].balance += parseInt(req.body.amount);
+
+    writeJSON('accounts.json');
+
+    res.render('transfer', { message: "Transfer Completed" });
+    // handleTransfer(req, res);
+});
 router.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }));
-router.post('/payment', (req, res) => handlePayment(req, res));
+router.post('/payment', (req, res) => {
+    accounts.credit.balance -= parseInt(req.body.amount);
+    accounts.credit.available += parseInt(req.body.amount);
+
+    writeJSON('accounts.json');
+
+    res.render('payment', { message: "Payment Successful", account: accounts.credit });
+    // handlePayment(req, res)
+});
 
 module.exports = router;
